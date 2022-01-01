12b55156a068d4e9fb24672f2b04ac94a2dc46a8
ValoOndrej <ondrejvalo6@gmail.com>
2022-01-01 11:45:48 -0800

    ov: heuristic change

--- dicewars/ai/xberez03_old.py
+++ dicewars/ai/xberez03_old.py
@@ -7,15 +7,19 @@ import random
 class AI(NEW_AI):
 
     def leaf_heuristic(self, board: Board, current_player: Name, players_order: List[Name], end_turn_gain: int):
-        h = [8 for _ in players_order]
+        h = [0 for _ in players_order]
         for index, player in enumerate(players_order):
-            if player == current_player:
-                h[index] = 0
-            else:
-                for area in board.get_player_border(player):
-                    for neighbor_area in area.get_adjacent_areas_names():
-                        if current_player == board.get_area(neighbor_area).get_owner_name():
-                            h[index] += 1
-            h[index] -= (board.get_player_dice(player) / 8)
-            h[index] -= (len(board.get_player_areas(player)) / 4)
+
+            h[index] = board.get_player_dice(player)
+            h[index] += len(board.get_player_areas(player) * 4)
+
+            players_regions = board.get_players_regions(player)
+            players_regions_sizes = []
+            for region in players_regions:
+                region_size = len(region)
+                players_regions_sizes.append(region_size)
+
+            h[index] += (max(players_regions_sizes) * 8)
+
         return h
+        
